d6f4bf1c579049dfc0e2e5a270d29a06235db293
Moheed-Gillani <68803885+Moheed-Gillani@users.noreply.github.com>
2020-09-04 11:56:57 +0300

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from .models import *
-
+admin.site.site_header = "keep your dick up"
+admin.site.site_title = "keep your dick up"
+admin.site.index_title = "keep your dick up"
 
 
 
