c7b3e77347306ebcacddb10590c24ad0c8021d48
Moheed <syedmoheedulhassan@gmail.com>
2020-09-04 10:55:28 +0300

    My Django Girls app, first commit

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 from .models import *
-admin.site.site_header = "keep your dick up"
-admin.site.site_title = "keep your dick up"
-admin.site.index_title = "keep your dick up"
+
+
+
 
 admin.site.register(Customer)
 admin.site.register(Product)
