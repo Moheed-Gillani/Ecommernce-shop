671d4e600eaf87af0e432b8a576e4fd8d1b106f2
Moheed <syedmoheedulhassan@gmail.com>
2020-09-04 09:06:48 +0300

    My Django Girls app, first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -120,11 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 MEDIA_URL = '/images/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 LOGIN_REDIRECT_URL = "store"
 LOGOUT_REDIRECT_URL = "store"
